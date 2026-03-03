549a44755b5e301874b9e586d52002cc29b111bb
adnan4456 <adnan4456@gmail.com>
2026-03-03 16:29:40 +0100

    changed artificat version from v2 to v3

--- app/src/main/java/com/example/musicplayer/MainActivity.kt
+++ app/src/main/java/com/example/musicplayer/MainActivity.kt
@@ -268,7 +268,8 @@ fun DrawerItem(item: NavigationItem, selected: Boolean, onItemClick: (Navigation
 
 @Composable
 fun Navigation(navController: NavHostController ,
-               audioViewModel :AudioViewModel = hiltViewModel()){
+               audioViewModel :AudioViewModel = hiltViewModel()
+){
 
     NavHost(navController, startDestination = NavigationItem.Home.route){
         composable(NavigationItem.Home.route){
